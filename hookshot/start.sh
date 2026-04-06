7d75070c408b47e89c9c95676fd365e46cde1e91
kimsokhomzZ <kimsokhom654@gmail.com>
2026-04-06 16:35:10 +0700

    Config nginx revers proxy & setup hookshot

---
+++ hookshot/start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+# Generate passkey if it doesn't exist yet (used to encrypt stored tokens)
+if [ ! -f /data/passkey.pem ]; then
+  echo "Generating passkey..."
+  openssl genpkey -out /data/passkey.pem -algorithm RSA -pkeyopt rsa_keygen_bits:4096
+fi
+
+# Substitute env vars into config and registration templates
+echo "Rendering hookshot config..."
+envsubst '${SERVER_NAME} ${SYNAPSE_INTERNAL_URL} ${LOG_LEVEL}' \
+  < /etc/hookshot/config.yaml.template > /data/config.yaml
+
+echo "Rendering hookshot registration..."
+envsubst '${SERVER_NAME} ${HOOKSHOT_AS_TOKEN} ${HOOKSHOT_HS_TOKEN}' \
+  < /etc/hookshot/registration.yaml.template > /data/registration.yaml
+
+echo "Starting Hookshot..."
+exec node /usr/bin/matrix-hookshot/Bridge.js \
+  --config /data/config.yaml \
+  --registration /data/registration.yaml \
+  --port 9993
--- synapse/start.sh
+++ synapse/start.sh
@@ -6,9 +6,11 @@ echo "Substituting environment variables..."
 envsubst '$SERVER_NAME $POSTGRES_USER $POSTGRES_PASSWORD $POSTGRES_HOST $POSTGRES_PORT $SYNAPSE_DB_NAME $MACAROON_KEY $FORM_SECRET $REGISTRATION_SHARED_SECRET $MAS_INTERNAL_URL $MAS_SHARED_SECRET $LIVEKIT_SERVICE_URL' \
     < /etc/synapse/homeserver.yaml.template > /data/homeserver.yaml
 
-# Added $TELEGRAM_BRIDGE_URL
 envsubst '$TELEGRAM_AS_TOKEN $TELEGRAM_HS_TOKEN $SERVER_NAME $TELEGRAM_BRIDGE_URL' \
-    < /etc/synapse/registration.yaml.template > /etc/synapse/registration.yaml
+    < /etc/synapse/telegram-registration.yaml.template > /etc/synapse/telegram-registration.yaml
+
+envsubst '$HOOKSHOT_AS_TOKEN $HOOKSHOT_HS_TOKEN $SERVER_NAME' \
+    < /etc/synapse/hookshot-registration.yaml.template > /etc/synapse/hookshot-registration.yaml
 
 export SYNAPSE_CONFIG_PATH=/data/homeserver.yaml
 export SYNAPSE_SERVER_NAME=$SERVER_NAME
