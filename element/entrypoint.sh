b69bfc382929f947f59f03a85a258abd1227b1d7
Sengly456 <senglyou84@gmail.com>
2026-04-06 16:55:13 +0700

    add entrypoint for docker

--- element/entrypoint.sh
+++ element/entrypoint.sh
@@ -5,4 +5,4 @@ envsubst '${SERVER_NAME} ${ELEMENT_WEB_CLIENT_ID}' \
   < /app/config.json.template \
   > /app/config.json
 
-exec nginx -g 'daemon off;'
+exec /docker-entrypoint.sh nginx -g 'daemon off;'
